21413e972440a72af242c757087864cb3cc88b65
Haesky <37891592+Haesky@users.noreply.github.com>
2018-03-30 09:43:24 +0200

    Add files via upload

---
+++ jeu2.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import time
+import random
+
+pos1 = 10 #X
+pos2 = 90 #Y
+pos3 = 60 #X
+pos4 = 140 #Y
+jumping = False
+
+fenetre = Tk()
+#canvas
+canvas = Canvas(fenetre, width=150, height=150, background='orange')
+
+#bonhomme
+pnj = canvas.create_oval(pos1, pos2, pos3, pos4)
+ligne1 = canvas.create_line(0,140, 150, 140)
+##saut du bonhomme
+def click(event):
+    global jumping
+    if jumping == False:
+        jump()
+        jumping = True
+
+def jump():
+    global pos2,pos4
+    if pos2 != 60:
+        canvas.coords(pnj,pos1,pos2,pos3,pos4)
+        pos2 -= 1
+        pos4 -= 1
+        canvas.after(10,jump)
+    else:
+        canvas.after(10, fall)
+
+def fall():
+    global pos2,pos4, jumping
+    if pos2 != 90:
+        canvas.coords(pnj,pos1,pos2,pos3,pos4)
+        pos2 += 1
+        pos4 += 1
+        canvas.after(10,fall)
+    else:
+        jumping = False
+
+fenetre.bind("<space>", click)
+
+#bouton
+bouton=Button(fenetre, text="Fermer", command=fenetre.destroy)
+
+#pack
+bouton.pack()
+canvas.pack()
+
+fenetre.mainloop()
